a07c82a88fc742035a3257db96cd319f12ca1d8a
Aurelien <a_prunell@etu-webschoolfactory.fr>
2022-06-20 11:24:17 +0200

    [FIX] ESlint error

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,17 @@
+import React from 'react';
+// import Link from 'react-router-dom';
+
 function NotFound() {
+  // const notFoundStyle = {
+  //   backgroundColor = "#C46013",
+  //   height = "100vh",
+  //   width = "100vw",
+  //   textAlign = "center",
+  // }
   return (
-    <div className="NotFound">
-      <p>NOT FOUUUUUND</p>
+    <div className="NotFound" >
+      <p>404 - Not Found</p>
+      {/* <Link to="/">Cliquez ici pour retourner sur la page d'accueil</Link> */}
     </div>
   );
 }
