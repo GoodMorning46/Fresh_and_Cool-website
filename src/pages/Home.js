8fbed6d915c5fd3a1862e7130937fbc9e127d9bd
hugues <h_bernarde@etu-webschoolfactory.fr>
2022-06-24 01:25:26 +0200

    [FUNC] Ajout Faker + Front update (home / footer)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./CSS/home.css";
 import Header from "../component/header";
 import Footer from "../component/footer";
+import Product from "../component/product";
 
 //import Navigation from "../component/Navigation";
 // <Navigation/>
@@ -10,6 +12,20 @@ function Home() {
     <div className="Home" id="Home">
       <Header />
       <div>
+        <div className="products flexer">
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+        </div>
         <p> test5</p>
       </div>
       <Footer />
