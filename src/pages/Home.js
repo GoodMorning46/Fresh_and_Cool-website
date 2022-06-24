47ebbfe65cb62bd341c63bafac943fc596bbdcda
Benji-Dev <benjilanery@gmail.com>
2022-06-24 19:18:26 +0200

    [MAJ CARD] Mise au propre des cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,15 +19,16 @@ function Home() {
           h2="Le tee-shirt"
           p="Ce tee-shirt vous procure une sensation de fraicheur tout au long de votre journée."
         />
-        <Card  // Le pare-soleil
-        img="https://pullfr-4c63.kxcdn.com/pare-soleil-de-voiture-127-6-26654.png?cultureCode=fr-FR&UseBlobStorage=False&version=9eeaf768280ade624315f0908b59657d"
-        h2="Le pare-soleil" 
-        p="Ce pare-soleil conçu avec une nouvelle technologie permettra à votre habitacle de rester frais peut importe la température extérieur."
+        <Card // Le pare-soleil
+          img="https://pullfr-4c63.kxcdn.com/pare-soleil-de-voiture-127-6-26654.png?cultureCode=fr-FR&UseBlobStorage=False&version=9eeaf768280ade624315f0908b59657d"
+          h2="Le pare-soleil"
+          p="Ce pare-soleil conçu avec une nouvelle technologie permettra à votre habitacle de rester frais peut importe la température extérieur."
         />
         <Card // La gourde
-        img="https://cdn3.sans-bpa.com/16350-large_default/qwetch-bouteille-inox-750-turquoise.jpg"
-        h2="La gourde"
-        p="Terminer avec l'eau chaude au fond du sac après une rando, la fraicheur n'a pas de prix" />
+          img="https://cdn3.sans-bpa.com/16350-large_default/qwetch-bouteille-inox-750-turquoise.jpg"
+          h2="La gourde"
+          p="testTerminer avec l'eau chaude au fond du sac après une rando, la fraicheur n'a pas de prix"
+        />
       </div>
       <Footer />
     </div>
