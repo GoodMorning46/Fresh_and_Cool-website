d20574ed226778c5147c106b7a2cec6ab6ed8f24
Benji-Dev <benjilanery@gmail.com>
2022-06-26 11:56:26 +0200

    [MODIF FILTER] modification du filter

--- src/component/card.js
+++ src/component/card.js
@@ -24,25 +24,25 @@ import React from "react";
 const Card = ({ item }) => {
   return (
     <>
-        <div className="card_body">
-          {item.map((Val) => {
-            return (
-              <div
-                className="col-md-4 col-sm-6 card my-3 py-3 border-0"
-                key={Val.id}
-              >
-                <div className="card-img-top text-center">
-                  <img src={Val.img} alt={Val.title} className="photo w-75" />
-                </div>
-                  <div className="card-title fw-bold fs-4">
-                    {Val.title} &nbsp;&nbsp;&nbsp;&nbsp;--&nbsp;&nbsp;
-                    {Val.price}
-                  </div>
-                  <div className="card-text">{Val.desc}</div>
-                </div>
-            );
-          })}
-        </div>
+      <div className="card_body">
+        {item.map((Val) => {
+          return (
+            <div
+              className="col-md-4 col-sm-6 card my-3 py-3 border-0"
+              key={Val.id}
+            >
+              <div className="card-img-top text-center">
+                <img src={Val.img} alt={Val.title} className="photo w-75" />
+              </div>
+              <div className="card-title fw-bold fs-4">
+                {Val.title} &nbsp;&nbsp;&nbsp;&nbsp;--&nbsp;&nbsp;
+                {Val.price}
+              </div>
+              <div className="card-text">{Val.desc}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
--- src/pages/event.js
+++ src/pages/event.js
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../component/header";
 import Footer from "../component/footer";
 import "./CSS/event.css";
 
-import Data from "../Data";
-import Card from "../component/card";
-
 const Event = () => {
-  const [item, setItem] = useState(Data);
   return (
-    <>
+    <div>
       <Header />
-      <div className="container-fluid">
-        <div className="row">
-          <h1 className="col-12 text-center my-3 fw-bold">Our Menu</h1>
-          <Card item={item} /> // UI Component
-        </div>
+      <div className="event">
+        <p>Test event</p>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
